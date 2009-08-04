96031f177265af356eb86f95231917388ca3daac
Beat Seeliger <bseelige@selma.(none)>
2009-08-04 18:06:03 +0200

    printf example

---
+++ cintro/10_printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("int: %d\n", 42);
+  printf("float: %f\n", 3.141);
+  printf("char: %c\n", 'z');
+  printf("string: %s\n", "abc");
+  printf("43d to oct: %o\n", 43);
+  printf("43d to dec: %x\n", 43);
+  printf("percent: %%\n");
+  return 0;
+}
+
+     
+
