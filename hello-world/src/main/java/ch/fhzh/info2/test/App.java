db1b411d7822e4532c0fa21b0caa66a92663b36e
Lukas Eppler <godot@sunray179.hsz-t.ch>
2010-02-22 20:09:53 +0100

    Testprojekt

---
+++ hello-world/src/main/java/ch/fhzh/info2/test/App.java
@@ -0,0 +1,13 @@
+package ch.fhzh.info2.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
