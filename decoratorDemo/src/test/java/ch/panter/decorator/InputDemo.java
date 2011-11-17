e6d6245e12c384545d41e808ba6b66937e7f1acb
seb <seb@panter.ch>
2011-11-17 15:50:05 +0100

    added decorator pattern demo

---
+++ decoratorDemo/src/test/java/ch/panter/decorator/InputDemo.java
@@ -0,0 +1,26 @@
+package ch.panter.decorator;
+
+import java.io.*;
+
+public class InputDemo {
+	public static void main(String[] args) throws IOException {
+		int c;
+
+		try {
+
+			InputStream in = new FileInputStream("src/test/resources/test.txt");
+
+			// in = new BufferedInputStream(in);
+			
+			// in = new LowerCaseInputStream(in);
+
+			while ((c = in.read()) >= 0) {
+				System.out.print((char) c);
+			}
+
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
